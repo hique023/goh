a162f5e84782a0a37f23f04a0bce07c35feead08
hique023 <pedroviski10@hotmail.com>
2021-04-05 08:36:08 -0300

    Criado estrutura inicial da página de login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,53 @@
-import react from 'react'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
 
-export default function Login() {
+// CSS
+import './styles.css'
+
+// Assets
+import login from '../../assets/login.png'
+
+export default function Logon() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const history = useHistory()
+
+    async function handleLogin(e) {
+        e.preventDefault()
+        history.push('/home')
+    }
+
     return (
-        <h1>Login</h1>
+        <div className="logon-container">
+            <section className="form">
+
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu login</h1>
+
+                    <input
+                        placeholder='E-mail'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <input
+                        placeholder='Senha'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button className="button" type='submit'>Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+
+            </section>
+            <img src={login} alt="Heroes" />
+        </div>
     )
 }
