295bfe294b867ace988bde2cf5b6323f3980d194
hique023 <pedroviski10@hotmail.com>
2021-04-10 14:01:09 -0300

    Finalizado tela Phase e componente StageLink

--- src/components/StageLink/index.js
+++ src/components/StageLink/index.js
@@ -1,12 +1,18 @@
 // Assets
 import './styles.css'
+import { BiChalkboard } from "react-icons/bi";
 
 export default function StageLink(props) {
     return (
         <div className="containerStageLink">
             <div className="contentStageLink">
-                <h1>{props.name}</h1>
-                <h1>400 xp</h1>
+                <button>
+                    <div className="leftStageLink">
+                        <BiChalkboard size={80} color="#e02041" />
+                        <h1>{props.name}</h1>
+                    </div>
+                    <h1>400 xp</h1>
+                </button>
             </div>
         </div>
     )
