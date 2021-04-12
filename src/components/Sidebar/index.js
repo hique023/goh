733255fdf1ef1d256cb32b1d374feeaafa14e6bb
hique023 <pedroviski10@hotmail.com>
2021-04-12 11:57:36 -0300

    Configurado Sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,25 @@
 // Global
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // Assets
 import './styles.css'
+import { ImHome } from "react-icons/im";
+import { GiPodium } from "react-icons/gi";
 
 export default function Sidebar() {
     return (
         <div className="containerSidebar">
-            <h1>Sidebar</h1>
+            <div className="contentSidebar">
+                <h1>Content Sidebar</h1>
+                <Link to="/home">
+                    <ImHome className="svgSelect" size={60} color="black" />
+                </Link>
+
+                <Link to="/ranking">
+                    <GiPodium size={60} color="black" />
+                </Link>
+            </div>
         </div>
     )
 }
