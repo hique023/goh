c4caec40b869d8455cdae6399c8f36f35b988ce1
hique023 <pedroviski10@hotmail.com>
2021-04-21 20:56:17 -0300

    Instalado Fireabse e criado arquivo de configuração

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import firebase from '@firebase/app'
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "APP_ID"
+}
+
+firebase.initializeApp(firebaseConfig)
+export default firebase
