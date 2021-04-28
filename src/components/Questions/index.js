a517bd0d6ae4722b4e0b94e4bb464f35b2194805
hique023 <pedroviski10@hotmail.com>
2021-04-28 11:36:39 -0300

    Criado estrutura para questões

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -1,7 +1,64 @@
+// Global
+import React, { useState } from 'react'
+
+// Assets
+import './styles.css'
+
 export default function Questions() {
+
+    const [question, setQuestion] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum corrupti qui voluptatem quam enim exercitationem accusantium porro, ex quae, fugiat aliquid, laborum aspernatur commodi dicta odit maiores id iusto. Numquam?')
+    const [correctAnswer, setCorrectAnswer] = useState('')
+    const [answerSelected, setAnswerSelected] = useState('')
+
     return (
         <div className="containerQuestions">
-            <h1>Questions</h1>
+            <div className="contentQuestions">
+                <h1>{question}</h1>
+            </div>
+            <div className="contentAnswers">
+                <div>
+                    <input
+                        type="radio"
+                        name="answers"
+                        id='answer1'
+                        value='answer1'
+                        onChange={(e) => { setAnswerSelected(e.target.value) }} />
+
+                    <label htmlFor="answer1">Answer 1</label>
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        name="answers"
+                        id='answer2'
+                        value='answer2'
+                        onChange={(e) => { setAnswerSelected(e.target.value) }} />
+
+                    <label htmlFor="answer2">Answer 2</label>
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        name="answers"
+                        id='answer3'
+                        value='answer3'
+                        onChange={(e) => { setAnswerSelected(e.target.value) }} />
+
+                    <label htmlFor="answer3">Answer 3</label>
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        name="answers"
+                        id='answer4'
+                        value='answer4'
+                        onChange={(e) => { setAnswerSelected(e.target.value) }} />
+
+                    <label htmlFor="answer4">Answer 4</label>
+                </div>
+            </div>
+
+            <h3>Resposta selecionada: {answerSelected}</h3>
         </div>
     )
 }
