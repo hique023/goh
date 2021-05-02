e4380d26ff62a198d8f8665f9af98d03d1977671
hique023 <pedroviski10@hotmail.com>
2021-05-02 10:06:10 -0300

    Finalizado lógica para recuperar questões

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -7,12 +7,13 @@ import './styles.css'
 
 export default function Questions() {
 
+    const [data, setData] = useState(null)
     const [question, setQuestion] = useState('Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum corrupti qui voluptatem quam enim exercitationem accusantium porro, ex quae, fugiat aliquid, laborum aspernatur commodi dicta odit maiores id iusto. Numquam?')
-    // const [correctAnswer, setCorrectAnswer] = useState('')
     const [answer1, setAnswer1] = useState('')
     const [answer2, setAnswer2] = useState('')
     const [answer3, setAnswer3] = useState('')
     const [answer4, setAnswer4] = useState('')
+    const [correctAnswer, setCorrectAnswer] = useState('')
     const [answerSelected, setAnswerSelected] = useState('')
     const [count, setCount] = useState(1)
     const color = localStorage.getItem('color')
@@ -36,26 +37,42 @@ export default function Questions() {
         docRef.get().then((doc) => {
             if (doc.exists) {
                 const data = doc.data()
-                const text = `pergunta${count}`
-                console.log("Document data:", data);
+                setData(data)
 
                 console.log('-----------------------------');
-                setQuestion(doc.data().pergunta4.pergunta)
-                const question = data.text.pergunta
-                const answer1 = data.text.resposta1
-                const answer2 = data.text.resposta2
-                const answer3 = data.text.resposta3
-                const answer4 = data.text.resposta4
+                console.table("Document data:", data);
+                console.table("Document data:", data[1]);
+
+                const pergunta = data[count]
+                console.table(pergunta)
+                console.log('-----------------------------');
+
+                const question = pergunta[0]
+                const answer1 = pergunta[1]
+                const answer2 = pergunta[2]
+                const answer3 = pergunta[3]
+                const answer4 = pergunta[4]
+                const correct = pergunta[5]
                 console.log(question);
                 console.log(answer1);
                 console.log(answer2);
                 console.log(answer3);
                 console.log(answer4);
+                console.log(correct);
+                setQuestion(question)
                 setAnswer1(answer1)
                 setAnswer2(answer2)
                 setAnswer3(answer3)
                 setAnswer4(answer4)
-                console.log('-----------------------------');
+                setCorrectAnswer(correct)
+
+                // console.log(question);
+                // console.log(answer1);
+                // console.log(answer2);
+                // console.log(answer3);
+                // console.log(answer4);
+                // console.log(correctAnswer);
+
             } else {
                 // doc.data() will be undefined in this case
                 console.log("No such document!");
@@ -63,12 +80,11 @@ export default function Questions() {
         }).catch((error) => {
             console.log("Error getting document:", error);
         });
-
     }
 
     useEffect(() => {
         getQuestions()
-    }, [])
+    }, [count])
 
     return (
         <div className="containerQuestions">
