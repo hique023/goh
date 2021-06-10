2289c15ee3c8d0a666eab12e2e607e20163e1c9a
hique023 <pedroviski10@hotmail.com>
2021-06-10 14:05:39 -0300

    Alterado status do quiz de respondido para bloqueado

--- src/components/QuizLink/index.js
+++ src/components/QuizLink/index.js
@@ -1,60 +1,57 @@
 // Global
-import React, { useEffect, useState } from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 // Assets
-import './styles.css'
+import "./styles.css";
 import { FiEdit } from "react-icons/fi";
 
 export default function QuizLink(props) {
+  // const [quizScore, setQuizScore] = useState(0)
+  const history = useHistory();
 
-    // const [quizScore, setQuizScore] = useState(0)
-    const history = useHistory()
+  // function getScore() {
+  //     setQuizScore(props.quizScore)
+  // }
 
-    // function getScore() {
-    //     setQuizScore(props.quizScore)
-    // }
+  function goQuiz() {
+    history.push("/quiz");
+  }
 
-    function goQuiz() {
-        history.push('/quiz')
-    }
+  const cardStyle = {
+    backgroundColor: props.color || "var(--green-soft)",
+  };
 
-    const cardStyle = {
-        backgroundColor: props.color || 'var(--green-soft)'
-    }
+  // useEffect(() => {
+  //     getScore()
+  // }, [])
 
-    // useEffect(() => {
-    //     getScore()
-    // }, [])
+  return (
+    <div className="containerQuizLink" style={cardStyle}>
+      <div className="contentQuizLink">
+        <button onClick={goQuiz} disabled={props.statusQuiz}>
+          <div className="leftQuizLink">
+            {props.statusQuiz === false ? (
+              <FiEdit size={80} color="var(--green-soft)" />
+            ) : (
+              <FiEdit size={80} color="var(--block)" />
+            )}
 
-    return (
-        <div className="containerQuizLink" style={cardStyle}>
-            <div className="contentQuizLink">
-                <button onClick={goQuiz} disabled={props.statusQuiz}>
-                    <div className="leftQuizLink">
-
-                        {props.statusQuiz === false ? (
-                            <FiEdit size={80} color="var(--green-soft)" />
-                        ) : (
-                            <FiEdit size={80} color="var(--block)" />
-                        )}
-
-                        {props.statusQuiz === false ? (
-                            <div>
-                                <h1>{props.name}</h1>
-                                <h1>(Pendente)</h1>
-                            </div>
-                        ) : (
-                            <div>
-                                <h1>{props.name}</h1>
-                                <h1>(Respondido)</h1>
-                            </div>
-                        )}
-
-                    </div>
-                    <h1>{props.quizScore} xp</h1>
-                </button>
-            </div>
-        </div>
-    )
+            {props.statusQuiz === false ? (
+              <div>
+                <h1>{props.name}</h1>
+                <h1>(Pendente)</h1>
+              </div>
+            ) : (
+              <div>
+                <h1>{props.name}</h1>
+                <h1>(Bloqueado)</h1>
+              </div>
+            )}
+          </div>
+          <h1>{props.quizScore} xp</h1>
+        </button>
+      </div>
+    </div>
+  );
 }
