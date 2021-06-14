7bb406c01db90d13c0f97a8ecfe2c77e94bbf3cf
hique023 <pedroviski10@hotmail.com>
2021-06-14 13:19:17 -0300

    Alterado cor dos botões de ação no modal para iniciar quiz

--- src/components/QuizStartModal/index.js
+++ src/components/QuizStartModal/index.js
@@ -1,35 +1,42 @@
 // Global
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 // Assets
-import './styles.css'
+import "./styles.css";
 
 export default function QuizStartModal(props) {
+  const history = useHistory();
 
-    const history = useHistory()
+  function start() {
+    props.startQuiz();
+  }
 
-    function start() {
-        props.startQuiz()
-    }
+  function cancel() {
+    props.cancelQuiz();
+  }
 
-    function cancel() {
-        props.cancelQuiz()
-    }
-
-    return (
-        <div className="backgroundQuizStartModal">
-            <div className="containerQuizStartModal">
-                <div className="containerHeader">
-                    <header>Deseja iniciar o Quiz?</header>
-                    <h2>Você terá apenas uma chance, verifique sua conexão com a internet e reserve 10min para a realização do quiz. Ao todo são 10 questões com níveis de dificuldade diferentes.</h2>
-                </div>
-
-                <div className="containerOptions">
-                    <button type="button" className="buttonStart" onClick={start}>Sim</button>
-                    <button type="button" className="buttonClose" onClick={cancel}>Não</button>
-                </div>
-            </div>
+  return (
+    <div className="backgroundQuizStartModal">
+      <div className="containerQuizStartModal">
+        <div className="containerHeader">
+          <header>Deseja iniciar o Quiz?</header>
+          <h2>
+            Você terá apenas uma chance, verifique sua conexão com a internet e
+            reserve 10min para a realização do quiz. Ao todo são 10 questões com
+            níveis de dificuldade diferentes.
+          </h2>
         </div>
-    );
+
+        <div className="containerOptions">
+          <button type="button" className="buttonStartQuiz" onClick={start}>
+            Sim
+          </button>
+          <button type="button" className="buttonCloseQuiz" onClick={cancel}>
+            Não
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
