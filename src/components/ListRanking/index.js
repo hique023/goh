b7ceffb8bb0be055fd89b5488de5c9851711833d
hique023 <pedroviski10@hotmail.com>
2021-07-01 12:11:05 -0300

    Ajustado xp no Ranking

--- src/components/ListRanking/index.js
+++ src/components/ListRanking/index.js
@@ -1,21 +1,20 @@
 // Global
-import React from 'react'
+import React from "react";
 
 // Assets
-import './styles.css'
-import image from '../../assets/backgroundregister.png'
+import "./styles.css";
+import image from "../../assets/backgroundregister.png";
 
 export default function ListRanking({ score, name, avatar }) {
-    return (
-        <div className="containerListRanking">
-
-            <div className="containerListRankingLeft">
-                <img src={avatar} alt="Avatar" />
-                <h1>{name}</h1>
-            </div>
-            <div className="containerListRankingRight">
-                <h1>{score} px</h1>
-            </div>
-        </div>
-    )
+  return (
+    <div className="containerListRanking">
+      <div className="containerListRankingLeft">
+        <img src={avatar} alt="Avatar" />
+        <h1>{name}</h1>
+      </div>
+      <div className="containerListRankingRight">
+        <h1>{score} xp</h1>
+      </div>
+    </div>
+  );
 }
